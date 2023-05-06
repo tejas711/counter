83be927122c047c1a920ee400bfa912214d11c98
tejas711 <tezzs967@gmail.com>
2023-05-06 15:54:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [timeCount, setTimeCount] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    let timer;
+    if (timeCount > 0) {
+      timer = setInterval(() => {
+        setCurrentTime((prevTime) => {
+          if (prevTime === 0) {
+            clearInterval(timer);
+            return 0;
+          } else {
+            return prevTime - 1;
+          }
+        });
+      }, 1000);
+    } else {
+      setCurrentTime(0);
+    }
+    return () => clearInterval(timer);
+  }, [timeCount]);
+
+  function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      const inputTime = Math.floor(parseFloat(event.target.value));
+      if (isNaN(inputTime)) {
+        setCurrentTime(0);
+      } else {
+        setTimeCount(inputTime);
+        setCurrentTime(inputTime);
+      }
+      event.target.value = "";
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" placeholder="Enter time in seconds" onKeyDown={handleKeyDown} />
+      <div id="current-time">{currentTime}</div>
     </div>
   );
 }
